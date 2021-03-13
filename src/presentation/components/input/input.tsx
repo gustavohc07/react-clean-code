eb2f1219dc94c75ebaeef9e53e03f1ed146b7e94
Gustavo Carvalho <gustavo.hcarvalho@outlook.com>
2021-03-13 15:28:53 -0300

    feat: ensure Login present valid state if password is valid

--- src/presentation/components/input/input.tsx
+++ src/presentation/components/input/input.tsx
@@ -17,11 +17,11 @@ const Input: React.FC<Props> = (props: Props) => {
     })
   }
   const getStatus = (): string => {
-    return '🔴'
+    return error ? '🔴' : '🟢'
   }
 
   const getTitle = (): string => {
-    return error
+    return error || 'Tudo Certo!'
   }
 
   return (
